918238ab9db24ee3a70ec7fe23226b64c63d2d02
daslef <daslef93@gmail.com>
2023-09-18 22:17:38 +0300

    delete unused function preprocess

--- main.py
+++ main.py
@@ -12,18 +12,12 @@ def load(path: str, encoding: str = "utf-8") -> str:
     return data
 
 
-def preprocess(text: str) -> str:
-    # добавить энтити рекогнишн
-    return text
-
-
 def tokenize(text: str) -> list[str]:
     return re.findall(r"\w{2,}", text)
 
 
 if __name__ == "__main__":
     text = load("data/wizard_oz.txt", encoding="cp1251")
-    text = preprocess(text)
     words = without_stopwords(tokenize(text))
     nouns = normalize(get_nouns(words))
     print(count_words__counter(nouns))
